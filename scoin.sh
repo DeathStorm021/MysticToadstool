7df762579cf2011920f169fcf4a4a61272d7c47f
DeathStorm021 <137414020+DeathStorm021@users.noreply.github.com>
2023-09-07 18:01:11 +0530

    Update scoin.sh

--- scoin.sh
+++ scoin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 host="https://www.universal-cdn.com"
-hanime_email=${EMAIL:-"$1"}
-hanime_password=${PASSWORD:-"$2"}
+hanime_email=$EMAIL
+hanime_password=$PASSWORD
 
 
 getSHA256() {
